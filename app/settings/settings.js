65809e2efdea1c7a7952def23dbcf100c6f82bd7
Dražen Odobašić <dodobas@candela-it.com>
2016-06-08 10:45:31 +0200

    Use DNS for da-tiles server

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -1,4 +1,4 @@
 export default {
-  'vt-source': 'http://52.207.244.74', // source of current vector tiles
-  'vt-hist-source': 'http://52.207.244.74', // source of historic vector tiles for compare feature
+  'vt-source': 'http://da-tiles.osm-analytics.org', // source of current vector tiles
+  'vt-hist-source': 'http://da-tiles.osm-analytics.org', // source of historic vector tiles for compare feature
 }
